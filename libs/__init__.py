2ecb5595419af7c67c409f0ea5d9ab2b76022d4d
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2018-07-20 23:04:44 +0530

    Package initialization

---
+++ libs/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Package initialization.
+Created on Fri Jul 20 11:00:00 2018
+Author: Prasun Roy | CVPRU-ISICAL (http://www.isical.ac.in/~cvpr)
+GitHub: https://github.com/prasunroy/cnn-on-degraded-images
+
+"""
+
+
+__version__ = '0.1.0'
